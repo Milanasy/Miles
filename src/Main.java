f5afa6e44bd78af13265ca1cff08a93d1c5b758c
Anastasiia Rakhmanova <rar.anastasiya@gmail.com>
2023-05-31 22:42:17 +0300

    changed milesPerRuble

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         double ticketPrice = 14770;
-        int milesPerRuble = 10;
+        int milesPerRuble = 20;
 
         int miles = (int) (ticketPrice / milesPerRuble);
+
         System.out.println(miles);
     }
-
 }
